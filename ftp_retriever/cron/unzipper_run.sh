9a1fda13699003a232b85345e89b61e8b37e8244
Stephen Donegan <sdonegan@host293.jc.rl.ac.uk>
2019-11-13 12:31:48 +0000

    removing ftp_retriever as need to make that a separate git repo

--- ftp_retriever/cron/unzipper_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/group_workspaces/cems2/slstr_cpa/software/ftp_retriever/unzipper.sh /group_workspaces/cems2/slstr_cpa/software/ftp_retriever/config/slstr_cpa_viscal_s3a.cfg >/dev/null 2>&1
